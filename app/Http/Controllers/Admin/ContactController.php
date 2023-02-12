42672bd376abb4ffad2facd67d3b3380b2fbe2be
nayeem <nayeemsarwar1275@gmail.com>
2023-02-12 16:17:57 +0600

    update

--- app/Http/Controllers/Admin/ContactController.php
+++ app/Http/Controllers/Admin/ContactController.php
@@ -4,21 +4,56 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Contact;
 
 class ContactController extends Controller
-{
+ {
+     public $user;
+    protected $contact;
+     public function __construct(Contact $contact)
+    {
+        $this->contact     = $contact;
+       $this->middleware(function ($request, $next) {
+             $this->user = Auth::guard('admin')->user();
+            return $next($request);
+        });     }
     public function index()
     {
+       if (is_null($this->user) || !$this->user->can('admin.contact.index')) {
+            abort(403, 'Sorry !! You are Unauthorized.');
+       }
+
+        $data['title']  = 'Contact';
+        $data['rows']   =  Contact::with('category')->orderBy('id', 'desc')->get();
+        // dd($data['rows']);
         return view('admin.contact.index');
-    }
+   }
 
-    public function view()
-    {
-        return view('admin.contact.view');
-    }
+//     public function view()
+//     {
+//         if (is_null($this->user) || !$this->user->can('admin.contact.view')) {
+//             abort(403, 'Sorry !! You are Unauthorized.');
+//         }
 
-    public function delete()
-    {
-        return view('admin.contact.view');
-    }
+
+//         $data['title'] = 'Contact Edit';
+//         $data['row'] = Contact::find($id);
+//         return view('admin.contact.view', compact('data'));
+//     }
+
+//     public function delete($id)
+//     {
+//         if (is_null($this->user) || !$this->user->can('admin.contact.delete')) {
+//             abort(403, 'Sorry !! You are Unauthorized.');
+//         }
+    
+//         $contact = Contact::find($id);
+        
+//         $contact->delete();
+        
+//         Toastr::success(trans('Contact Deleted Successfully !'), 'Success', ["positionClass" => "toast-top-center"]);
+//         return view('admin.contact.view');
+
+//     }
 }
--- resources/views/admin/contact/index.blade.php
+++ resources/views/admin/contact/index.blade.php
@@ -1,72 +1,98 @@
 @extends('admin.layouts.master')
-@section('title') {{ $data['title'] ?? '' }} @endsection
+@section('title')
+    {{ $data['title'] ?? '' }}
+@endsection
 @section('content')
-<div class="content-wrapper">
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0">Contact</h1>
-                </div>
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
-                        <li class="breadcrumb-item active">Contact</li>
-                    </ol>
+    <div class="content-wrapper">
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Contact</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
+                            <li class="breadcrumb-item active">Contact</li>
+                        </ol>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <div class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <div class="row align-items-center">
-                                <div class="col-6">
-                                    <h3 class="card-title">Manage Contact </h3>
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <div class="row align-items-center">
+                                    <div class="col-6">
+                                        <h3 class="card-title">Manage Contact </h3>
+                                    </div>
+
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="card-body table-responsive p-0">
-                            <table id="dataTables" class="table table-hover text-nowrap jsgrid-table">
-                                <thead>
-                                    <tr>
-                                        <th width="5%">SL</th>
-                                        <th width="10%">Name</th>
-                                        <th width="10%">Email</th>
-                                        <th width="10%">Phone</th>
-                                        <th width="15%">Reason for Inquiry</th>
-                                        <th width="15%">Message</th>
-                                        <th width="10%">Date</th>
-                                        <th width="10%">Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>01</td>
-                                        <td>Rabin</td>
-                                        <td>rabin@gmail.com</td>
-                                        <td>0124-9854-985</td>
-                                        <td>Advertising</td>
-                                        <td>Lorem ipsum dolor sit amet consectetur adipisicing elit......</td>
-                                        <td>01 Feb, 23</td>
-                                        <td>
-                                            <a href="" class="btn btn-primary">View</a>
-                                            <a href="#" id="deleteData" class="btn btn-danger">Delete</a>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
+                            <div class="card-body table-responsive p-0">
+                                <table id="dataTables" class="table table-hover text-nowrap jsgrid-table">
+                                    <thead>
+                                        <tr>
+                                            <th width="5%">SL</th>
+                                            <th width="10%">Name</th>
+                                            <th width="10%">Email</th>
+                                            <th width="10%">Phone</th>
+                                            <th width="15%">Reason for Inquiry</th>
+                                            <th width="15%">Message</th>
+                                            <th width="10%">Date</th>
+                                            <th width="10%">Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @if (isset($data['rows']) && count($data['rows']) > 0)
+                                            @foreach ($data['rows'] as $key => $row)
+                                                <tr>
+                                                    <td>{{ $key + 1 }}</td>
+                                                    <td>{{ $row->name }}</td>
+                                                    <td>{{ $row->email }}</td>
+                                                    <td>{{ $row->phone }}</td>
+                                                    <td>{{ $row->reason }}</td>
+                                                    <td>{{ $row->message }}</td>
+                                                    <td>
+                                                        @if ($row->status == 1)
+                                                            <span class="badge badge-success">Active</span>
+                                                        @else
+                                                            <span class="badge badge-danger">InActive</span>
+                                                        @endif
+                                                    </td>
+                                                    <td>{{ date('d M Y', strtotime($row->created_at)) }}</td>
 
+                                                    </td>
+                                                    <td>
+                                                        @if (Auth::user()->can('admin.contact.index'))
+                                                            <a href="{{ route('admin.contact.view', $row->id) }}"
+                                                                class="btn btn-primary">View</a>
+                                                        @endif
+                                                        @if (Auth::user()->can('admin.contact.index'))
+                                                            <a href="{{ route('admin.contact.edit', $row->id) }}"
+                                                                class="btn btn-secondary">Edit</a>
+                                                        @endif
+                                                        @if (Auth::user()->can('admin.contact.index'))
+                                                            <a href="{{ route('admin.contact.delete', $row->id) }}"
+                                                                id="deleteData" class="btn btn-xs btn-danger">Delete</a>
+                                                        @endif
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                        @endif
+                                    </tbody>
+                                </table>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
